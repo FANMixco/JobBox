d742a7135b517123735616a0a90f8a74848a451a
Federico Navarrete <fanmixco@gmail.com>
2012-09-13 20:03:18 -0600

    Job

--- application/views/forms/honorific/add.php
+++ application/views/forms/honorific/add.php
@@ -1,24 +1,4 @@
-<h1><?php echo $title; ?></h1><hr/><br/>
-<?php echo form_open(); ?>
-<table>
-    <tr>
-        <td>
-            <?php echo form_label("Pa&iacute;s"); ?>
-        </td>
-        <td><?php echo form_input('ncountry',$this->input->post('ncountry')); ?></td>
-        <td class="required"></td>
-    </tr>
-    <td colspan="2"><br/>
-        <?php echo anchor('admin',$this->lang->line('txt_go_back'),array('class' => 'btn')); ?>
-            <button type="submit" class="btn-black">
-            	<i class="icon icon-ok"></i>
-            	<?php echo $this->lang->line('txt_save'); ?>            	
-            </button>
-    </td>
-</table>
-<?php echo validation_errors('<h5 class="message message-error" style="max-width:45%">','</h5>'); ?>
-<br/>
-<?php echo form_close(); ?><h1><?php echo $title; ?></h1><hr/><br/>
+<?php echo $title; ?></h1><hr/><br/>
 <?php echo form_open(); ?>
 <table>
     <tr>
--- application/views/forms/job/add.php
+++ application/views/forms/job/add.php
@@ -1,7 +1,21 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<?php echo $title; ?></h1><hr/><br/>
+<?php echo form_open(); ?>
+<table>
+    <tr>
+        <td>
+            <?php echo form_label("Tratamiento"); ?>
+        </td>
+        <td><?php echo form_input('nhonorific',$this->input->post('nhonorific')); ?></td>
+        <td class="required"></td>
+    </tr>
+    <td colspan="2"><br/>
+        <?php echo anchor('admin',$this->lang->line('txt_go_back'),array('class' => 'btn')); ?>
+            <button type="submit" class="btn-black">
+            	<i class="icon icon-ok"></i>
+            	<?php echo $this->lang->line('txt_save'); ?>            	
+            </button>
+    </td>
+</table>
+<?php echo validation_errors('<h5 class="message message-error" style="max-width:45%">','</h5>'); ?>
+<br/>
+<?php echo form_close(); ?>
