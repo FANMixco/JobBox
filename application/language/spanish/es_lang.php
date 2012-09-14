02aa1890ee56e64d4b5c09a2d33d0f7f6347570f
Federico Navarrete <fanmixco@gmail.com>
2012-09-13 20:46:30 -0600

    Language
    Paquete de Idiomas terminado

--- application/language/spanish/es_lang.php
+++ application/language/spanish/es_lang.php
@@ -29,15 +29,89 @@ $lang['txt_enter'] 	              = "Entrar";
 //General
 $lang['lbl_user']	              = "Usuario:";
 $lang['lbl_pass']	              = "Contrase&ntilde;a:";
+$lang['lbl_id']	              = "ID:";
 
 //Users
 $lang['lbl_first_name']           = "Primer Nombre:";
 $lang['lbl_second_name']          = "Segundo Nombre:";
 $lang['lbl_first_surname']        = "Primer Apellido:";
 $lang['lbl_second_surname']       = "Segundo Apellido:";
+$lang['lbl_married_surname']       = "Apellido de Casada:";
 $lang['lbl_birth_date']     	  = "Fecha de Nacimiento:";
 $lang['lbl_pass_confirm']	      = "Confirmar Contrase&ntilde;a:";
+$lang['lbl_birth_city']       = "Ciudad de Nacimiento:";
+$lang['lbl_honorific']       = "Tratamiento:";
+$lang['lbl_sex']       = "Sexo:";
+$lang['lbl_religion']       = "Religi&iacute;n:";
+$lang['lbl_document']       = "DUI:";
+$lang['lbl_passport']       = "Pasaporte:";
+$lang['lbl_isss']       = "Seguro Social:";
+$lang['lbl_nit']       = "NIT:";
+$lang['lbl_height']       = "Altura:";
+$lang['lbl_weight']       = "Peso:";
+$lang['lbl_travel']       = "Disponibilidad para Viajar:";
+$lang['lbl_loc_travel']       = "Movilidad Nacional:";
+$lang['lbl_int_travel']       = "Movilidad Internacional:";
+$lang['lbl_salary']       = "Salario Min&iacute;mo:";
+$lang['lbl_job_area']       = "&Aacute;rea de Trabajo:";
+$lang['lbl_hobbies']       = "Pasatiempos:";
+$lang['lbl_others']       = "Otras denominaciones:";
+$lang['lbl_start_date']       = "Fecha de Inicio:";
+$lang['lbl_end_date']       = "Fecha de Fin:";
+$lang['lbl_school']       = "Centro Educativo:";
+$lang['lbl_level']       = "Nivel:";
+$lang['lbl_major']       = "T&iacute;tulo:";
+$lang['lbl_specialty']       = "Especialidad:";
+$lang['lbl_title_years']       = "A&ntilde;os de Titulaci&oacute;n:";
+$lang['lbl_complete_years']       = "A&ntilde;os de Completados:";
+$lang['lbl_city']       = "Ciudad:";
+$lang['lbl_address']       = "Direcci&oacute;n:";
+$lang['lbl_applicant_status']       = "Estatus de Aplicaci&oacute;n:";
+$lang['lbl_applicant_type']       = "Tipo de Aplicaci&oacute;n:";
+$lang['lbl_origin']       = "Origen:";
+$lang['lbl_recruiter']       = "Reclutador:";
+$lang['lbl_selection_source']       = "Fuente de Selecci&oacute;n:";
+$lang['lbl_contact']       = "Contacto:";
+$lang['lbl_chosen']       = "Elegido:";
+$lang['lbl_primary']       = "Contacto Principal:";
+$lang['lbl_reference']       = "Tipo de Referencia:";
+$lang['lbl_type_1']       = "Tipo de lÃ­nea(1):";
+$lang['lbl_type_2']       = "Tipo de lÃ­nea(2):";
+$lang['lbl_country_code_1']       = "Prefijo Pa&iacute;s(1):";
+$lang['lbl_country_code_2']       = "Prefijo Pa&iacute;s(2):";
+$lang['lbl_state_int_code_1']       = "Prefijo Internacional(1):";
+$lang['lbl_state_int_code_2']       = "Prefijo Internacional(2):";
+$lang['lbl_state_loc_code_1']       = "Prefijo Provincia(1):";
+$lang['lbl_state_loc_code_2']       = "Prefijo Provincia(2):";
+$lang['lbl_course']       = "Nombre del Curso:";
+$lang['lbl_hours']       = "N&uacute;mero de Horas:";
+$lang['lbl_scholarship']       = "Beca:";
+$lang['lbl_comments']       = "Comentarios:";
+$lang['lbl_email']       = "Correo electr&oacute;nico:";
+$lang['lbl_company']       = "Compa&ntilde;ia:";
+$lang['lbl_job_sector']       = "Sector:";
+$lang['lbl_telephone']       = "N&uacute;mero de Tel&eacute;fono:";
+$lang['lbl_employee_number']       = "Num&uacute;ero de Empleados:";
+$lang['lbl_start_position']       = "Puesto inicial:";
+$lang['lbl_end_position']       = "Puesto final:";
+$lang['lbl_activities']       = "Actividades:";
+$lang['lbl_boss']       = "Jefe Inmediato:";
+$lang['lbl_boss_position']       = "Cargo del Jefe:";
+$lang['lbl_reason']       = "Raz&oacute;n:";
+$lang['lbl_reason']       = "Raz&oacute;n:";
 
+//Countries
+$lang['lbl_country']       = "Pa&iacute;s:";
+
+//Languages
+$lang['lbl_languages']       = "Idiomas:";
+$lang['lbl_language']       = "Idioma:";
+
+//Jobs
+$lang['lbl_position_name']       = "Cargo:";
+$lang['lbl_description']       = "Descripci&oacute;n:";
+$lang['lbl_type']       = "Tipo:";
+$lang['lbl_requirements']       = "Requerimientos:";
 
 /*--------------------------------------------------------------------------------------------------------------------------*/
 /* MESSAGES 						 																						*/
@@ -45,6 +119,6 @@ $lang['lbl_pass_confirm']	      = "Confirmar Contrase&ntilde;a:";
 /*																															*/
 /*--------------------------------------------------------------------------------------------------------------------------*/
 $lang['msg_no_jobs']              = "No existe ninguna plaza de trabajo.";
-$lang['msg_register']             = "Registrese en JobBox y podrá aplicar a todas las plazas del sistema, participar en procesos de selección, subir su perfil y darse a conocer en el medio profesional.";
+$lang['msg_register']             = "Registrese en JobBox y podrï¿½ aplicar a todas las plazas del sistema, participar en procesos de selecciï¿½n, subir su perfil y darse a conocer en el medio profesional.";
 
 ?>
